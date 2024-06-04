c7871bab8e1a941ee513c737c04da9cff130a919
Mareteglitch <136013198+Mareteglitch@users.noreply.github.com>
2024-06-04 19:10:44 +0000

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,23 @@
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
-    return(
+    return (
         <header className="header">
             <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center
             justify-center font-bold flex shadow-md">
                 <p className="blue-gradient_text">YK</p>
             </NavLink>
             <nav className="flex text-lg gap-7 font-medium">
-        
-                <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+
+                <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                     About
                 </NavLink>
-                <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+                <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                     Projects
                 </NavLink>
-                <NavLink to="/contact" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
-                    Contact
-                </NavLink>
-
+            
             </nav>
-        </header>
+        </header >
     )
 }
 export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+
+{/*<div className='absolute top-28 left-0 right-0 z-10 flex justify-center items-center '>
+        POPUP
+      </div>*/}
+const Home = () => {
+  return (
+      
+    <section className = "w-full h-screen relative">  {/* this is the section for the screen*/}
+      <Canvas 
+      className='w-full h-screen bg-transparent'
+      camera={{near:0.1, far:1000}}
+      >
+      <Suspense fallback={<loader/>}> {/* render the loading screen */}
+
+      </Suspense>  
+
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home
